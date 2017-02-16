43ab7f69171b2f5f6dbf5be704f1a61e604d8cff
grapelove <grapelove79@gmail.com>
2017-02-17 00:56:17 +0900

    이미지 갤러

---
+++ Javascript/04_imageGallery/js/gallery.js
@@ -0,0 +1,2 @@
+var req = new XMLHttpRequest(); // MLHttpRequest()클래스의 req인스턴스 생성
+req.open("GET", "./json/gallery.json"); // 접근 연산자를 이용한 함수(메서드) 호출: 클래스 내부에 들어 있는 기능을 이용ㅎ기 위해서는  "인스턴스 생성 후 접근 연산자를 이용해 사용한다."
